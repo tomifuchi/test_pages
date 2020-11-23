17aa709821c71594f8fa939b3b65187079ca49a6
tomifuchi <tomifuchi@yahoo.com>
2020-11-23 11:36:16 +0700

    On branch master
    Initial commit
    
     Changes to be committed:
    	new file:   css/style.css
    	new file:   index.php
    	new file:   index_en.php
    	new file:   index_ja.php
    	new file:   project/acme.html
    	new file:   project/acme_style.css
    	new file:   resume/res_computer.pdf
    	new file:   resume/res_computer_en.pdf

---
+++ index_ja.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	 	<title>富縁のオンライン履歴書</title>
+	 	<link rel="stylesheet" href="css/style.css">
+	</head>
+
+	<body>
+	  	<div class="grid-outer">
+		  <header id="header">
+		     <div id="logo-wrap" >
+		       <h1 class="content-title">フ。ロク</h1>
+		     </div>
+		     <div id="navbar">
+		      <nav>
+		        <ul>
+		          <li><a class="nav-btn" href="resume/res_computer_en.pdf">履歴書</a></li>
+		          <li><a class="nav-btn" href="#demo-intro">制作</a></li>
+		          <li><a class="nav-btn" href="index_en.php">英語</a></li>
+		          <li><a class="nav-btn" href="index.php">ベトナム語</a></li>
+		        </ul>
+		      </nav>
+		     </div>
+		  </header>
+
+		 <main id="main">
+		 <!--Key viusal-->
+		  <div id="key-visual" >
+		    <div class="bg-image"></div>
+		    <div class="content-wrap">
+		      <h2 class="content-title">うちのサイトへよこそ</h2>
+			<p>プログランミンは難しいですが。できたら満足してしまいます</p>
+		      <a class="btn" href="#demo-intro">制作をショーケースする</a>
+		    </div>
+		  </div>
+		 <!--Project-->
+		  <section id="demo-intro">
+		   <div class="content-wrap">
+		    <h2 class="content-title">制作</h2>
+		    <p>フィクションサイト制作サービス会社のホームページ。これはCSSとHTMLの基本を練習するのです。</P>
+		   </div>
+
+		  </section>
+		  <section id="acme">
+		    <div class="bg-image"></div>
+		    <div class="content-wrap">
+		      <h2 class="content-title">Acme Web Solutions</h2>
+		      <a class="btn" href="project/acme.html">ショーケースする</a>
+		    </div>
+		  </section>
+		 </main>
+		<!--Footer-->
+		 <footer id="footer">
+		   <div class="content-wrap">
+		    <div>Written by <a>Phu Loc</a></div>
+		    <div>Copyright &copy; <?php echo date("Y")?></div>
+		    <div>FAMP stack with PHP version : <?php echo phpversion()?></div>
+		   </div>
+		 </footer>
+		 
+		</div> <!--Grid for the entire place-->
+	</body>
+</html>
