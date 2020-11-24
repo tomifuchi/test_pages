94f06ae8fc59ce84ceefba0f74e89230d64d75cb
tomifuchi <tomifuchi@yahoo.com>
2020-11-24 11:18:29 +0700

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   index.php

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
 	<head>
 	 	<title>Đặng Trần Phú Lộc's profile</title>
 	 	<link rel="stylesheet" href="css/style.css">
+
+		<!--These meta lines is really important-->
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+
 	</head>
 
 	<body>
